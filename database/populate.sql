ff8cac0676a1e53417edf45c97b48760d3a4e540
matmatias <matheusomr1@gmail.com>
2023-12-21 14:01:10 -0300

    feat: add mysql database
    Add mysql database via docker and scripts to populate and clear the
    database

---
+++ database/populate.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS employees 
+( 
+	id int AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(20) NOT NULL,
+  last_name VARCHAR(20) NOT NULL,
+	DEPARTMENT VARCHAR(50) NOT NULL,
+  age int NOT NULL
+);
+
+LOAD DATA INFILE "/var/lib/mysql-files/employees.csv"
+INTO TABLE employees
+FIELDS TERMINATED BY ","
+LINES TERMINATED BY "\n"
+IGNORE 1 LINES;
