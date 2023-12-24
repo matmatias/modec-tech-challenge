791d25834500c70820a48a0a2f4b7efdf5a9a48f
matmatias <matheusomr1@gmail.com>
2023-12-24 12:15:12 -0300

    feat(frontend): add employee search page structure
    This commits only the page structure, so it is still not integrated with
    the backend

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,113 +1,145 @@
-import Image from 'next/image'
+"use client";
+
+import type { Employee } from "@/model";
+import { Dispatch, Fragment, SetStateAction, useEffect, useState } from "react";
+
+type EmployeeFilter = Omit<Employee, "id" | "age"> & {
+  age: string;
+};
 
 export default function Home() {
+  const [employeeFilter, setEmployeeFilter] = useState<EmployeeFilter>({
+    first_name: "",
+    last_name: "",
+    department: "",
+    age: "",
+  });
+  const [filtersQty, setFiltersQty] = useState<number>(1);
+  // const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
+
+  console.log(employeeFilter);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <Fragment>
+      <header className="flex flex-row">
+        <div>MODEC</div>
+        <h1>Employee Search</h1>
+        <div>Welcome, Fulano!</div>
+      </header>
+      <div>English</div>
+      <main>
+        <div>
+          <p>Select Filters:</p>
+          <div className="flex flex-col">
+            {Array.from({ length: filtersQty }).map((_, index: number) => (
+              <div className="flex flex-row" key={index}>
+                <Filter
+                  employeeFilter={employeeFilter}
+                  setEmployeeFilter={setEmployeeFilter}
+                />
+                {isLastFilter(index, filtersQty) && (
+                  <div>
+                    <button
+                      disabled={isFiltersQtyMax(filtersQty)}
+                      onClick={() => setFiltersQty((prev) => prev + 1)}
+                    >
+                      +
+                    </button>
+                    <button
+                      disabled={isFiltersQtyMin(filtersQty)}
+                      onClick={() => setFiltersQty((prev) => prev - 1)}
+                    >
+                      -
+                    </button>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-row">
+            <button>Search</button>
+            <button>Clear</button>
+          </div>
         </div>
-      </div>
+      </main>
+    </Fragment>
+  );
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+enum SelectOptions {
+  FirstName = "first_name",
+  LastName = "last_name",
+  Department = "department",
+  Age = "age",
+}
+
+interface FilterProps {
+  employeeFilter: EmployeeFilter;
+  setEmployeeFilter: Dispatch<SetStateAction<EmployeeFilter>>;
+}
+
+function Filter({ employeeFilter, setEmployeeFilter }: FilterProps) {
+  const [selected, setSelected] = useState<keyof EmployeeFilter | "">("");
+
+  useEffect(() => {
+    return () => {
+      if (selected !== "") {
+        resetFilterProp(selected, setEmployeeFilter);
+      }
+    };
+  }, []);
+
+  return (
+    <Fragment>
+      <select
+        value={selected}
+        onChange={(evt) => {
+          if (selected !== "") {
+            resetFilterProp(selected, setEmployeeFilter);
+          }
+          setSelected(evt.target.value as SelectOptions);
+        }}
+      >
+        <option defaultValue="" disabled value={""}>
+          Select an option
+        </option>
+        <option value={SelectOptions.FirstName}>First Name</option>
+        <option value={SelectOptions.LastName}>Last Name</option>
+        <option value={SelectOptions.Department}>Department</option>
+        <option value={SelectOptions.Age}>Age</option>
+      </select>
+      {selected !== "" && (
+        <input
+          value={employeeFilter[selected]}
+          onChange={(evt) => {
+            const selectedFilterProp = { [selected]: evt.target.value };
+            setEmployeeFilter((prev) => ({
+              ...prev,
+              ...selectedFilterProp,
+            }));
+          }}
         />
-      </div>
+      )}
+    </Fragment>
+  );
+}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+function resetFilterProp(
+  selected: keyof EmployeeFilter,
+  setEmployeeFilter: Dispatch<SetStateAction<EmployeeFilter>>,
+): void {
+  const resetedFilterProp = { [selected]: "" };
+  setEmployeeFilter((prev) => ({ ...prev, ...resetedFilterProp }));
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+function isFiltersQtyMax(filtersQty: number): boolean {
+  return filtersQty < 4 ? false : true;
+}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+function isFiltersQtyMin(filtersQty: number): boolean {
+  return filtersQty > 1 ? false : true;
+}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+function isLastFilter(index: number, filtersQty: number): boolean {
+  return index === filtersQty - 1 ? true : false;
 }
