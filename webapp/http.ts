4a27e710cade9ce457d0c140e9b07f66c47b2765
matmatias <matheusomr1@gmail.com>
2023-12-25 23:24:41 -0300

    ops: add NODE_ENV env variable to determine the env type
    NEXT_PUBLIC_NODE_ENV can be "development" or "production"

--- webapp/http.ts
+++ webapp/http.ts
@@ -4,4 +4,7 @@ import { config } from "dotenv";
 const envPath = path.resolve(__dirname, "../.env");
 config({ path: envPath });
 
-export const WEBAPP_URL = `http://${process.env.NEXT_PUBLIC_WEBAPP_API_URL}:${process.env.NEXT_PUBLIC_WEBAPP_PORT}`;
+export const WEBAPP_URL =
+  process.env.NEXT_PUBLIC_NODE_ENV === "development"
+    ? `http://${process.env.NEXT_PUBLIC_WEBAPP_API_URL}:${process.env.NEXT_PUBLIC_WEBAPP_PORT}`
+    : `https://${process.env.NEXT_PUBLIC_WEBAPP_API_URL}`;
